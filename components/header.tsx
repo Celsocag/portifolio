1be74d7d0b4e2515f384102b3f882245cab8aff9
Celsocag <celsoalbugo@gmail.com>
2025-06-09 17:00:33 -0300

    fixing header mobile

--- components/header.tsx
+++ components/header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-900 dark:text-white transition-colors duration-300">
-              Alex Johnson
+              Celso Albuquerque
             </Link>
           </div>
 
